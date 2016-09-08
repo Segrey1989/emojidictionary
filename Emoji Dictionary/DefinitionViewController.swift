b9db96a5c8bae318822c4b1d053cee2aa8c5397c
Siarhei Kvachonak <segrey1989@tut.by>
2016-09-08 15:37:13 +0300

    Using classes

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Siarhei Kvachonak on 02.09.16.
+//  Copyright © 2016 Siarhei Kvachonak. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Emoji Dictionary/Emoji.swift
@@ -0,0 +1,9 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Siarhei Kvachonak on 07.09.16.
+//  Copyright © 2016 Siarhei Kvachonak. All rights reserved.
+//
+
+import Foundation
